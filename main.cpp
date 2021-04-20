3d0adc5771e6d5897e74d40152696612edb2dfad
Nevsky12 <78961896+Nevsky12@users.noreply.github.com>
2021-04-20 16:02:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 const int N = 50, V = 11, T = 2000;
 const double dv = 1.0, dx = 10.0, dt = 1.0;
 
-double get_V(int id) { return dv * (id - V / 2) - dv / 2; }
+double get_V(int id) { return dv * (id - int (V / 2)); }
 
 void set_initial_data(double other[N][V]) {
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < V; j++) {
-            auto v = get_V(j);
-            auto x = (i - N / 2);
-            other[i][j] = std::exp(-v * v / 2) * std::exp( - x*x / 2);
+            auto x = (i - N / 2) * dx;
+            if (j == 7) {
+                other[i][j] = std::exp(-x * x / 2);
+            } else {
+                other[i][j] = 1e-6;
+            }
         }
     }
 }
@@ -25,40 +28,45 @@ int main() {
     double ar_next[N][V]{0};
     double ar_concentration[N]{0};
     set_initial_data(ar_current);
+    double total_count = 0;
     std::fstream file_out;
     for (int k = 0; k < T; k++) {
         for (int i = 0; i < N; i++) {
 
             for (int j = 0; j < V; j++) {
-                auto v = get_V(j);
+                double v = get_V(j);
                 double gamma = v * dt / dx;
 
                 if (v > 0) {
                     if (i == 0) {
-                        ar_next[0][j] = ar_current[N-1][j];
+                        ar_next[i][j] = ar_current[i][V - j - 1];
                     } else {
                         ar_next[i][j] = ar_current[i][j] - gamma * (ar_current[i][j] - ar_current[i - 1][j]);
                     }
                 } else {
                     if (i == N - 1) {
-                        ar_next[N - 1][j] = ar_current[0][j];
+                        ar_next[i][j] = ar_current[i][V - j - 1];
                     } else {
                         ar_next[i][j] = ar_current[i][j] - gamma * (ar_current[i + 1][j] - ar_current[i][j]);
                     }
                 }
             }
         }
-        std::swap(ar_current, ar_next);
-
-        sprintf(filename, "data/out_%03d.txt", k);
-        file_out.open(filename, std::fstream::out);
-        for (int i = 0; i < N; i++) {
-            ar_concentration[i] = 0;
-            for (int j = 0; j < V; j++) {
-                ar_concentration[i] += ar_current[i][j];
+        std::swap(ar_next, ar_current);
+        if (k % 10 == 0) {
+            sprintf(filename, "data/out_%03d.txt", k);
+            file_out.open(filename, std::fstream::out);
+            total_count = 0;
+            for (int i = 0; i < N; i++) {
+                ar_concentration[i] = 0;
+                for (int j = 0; j < V; j++) {
+                    ar_concentration[i] += ar_current[i][j];
+                }
+                file_out << i << '\t' << ar_concentration[i] << std::endl;
+                total_count += ar_concentration[i];
             }
-            file_out << i << '\t' << ar_concentration[i] << std::endl;
+            std::cout << total_count <<std::endl;
+            file_out.close();
         }
-        file_out.close();
     }
 }
